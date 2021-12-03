269b3230b49d35f62168cadcd5a4490c76ef6215
takovoe <takovoe@gmail.com>
2021-12-03 19:43:23 +0500

    sapr

---
+++ Sapr/Form5.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace Sapr
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text1 = new System.Windows.Forms.Label();
+            this.text2 = new System.Windows.Forms.Label();
+            this.text4 = new System.Windows.Forms.Label();
+            this.text3 = new System.Windows.Forms.Label();
+            this.text5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // text1
+            // 
+            this.text1.AutoSize = true;
+            this.text1.Location = new System.Drawing.Point(304, 88);
+            this.text1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(35, 13);
+            this.text1.TabIndex = 0;
+            this.text1.Text = "label1";
+            // 
+            // text2
+            // 
+            this.text2.AutoSize = true;
+            this.text2.Location = new System.Drawing.Point(304, 140);
+            this.text2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(35, 13);
+            this.text2.TabIndex = 1;
+            this.text2.Text = "label1";
+            // 
+            // text4
+            // 
+            this.text4.AutoSize = true;
+            this.text4.Location = new System.Drawing.Point(304, 233);
+            this.text4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.text4.Name = "text4";
+            this.text4.Size = new System.Drawing.Size(35, 13);
+            this.text4.TabIndex = 3;
+            this.text4.Text = "label1";
+            // 
+            // text3
+            // 
+            this.text3.AutoSize = true;
+            this.text3.Location = new System.Drawing.Point(304, 184);
+            this.text3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.text3.Name = "text3";
+            this.text3.Size = new System.Drawing.Size(35, 13);
+            this.text3.TabIndex = 2;
+            this.text3.Text = "label1";
+            // 
+            // text5
+            // 
+            this.text5.AutoSize = true;
+            this.text5.Location = new System.Drawing.Point(304, 280);
+            this.text5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.text5.Name = "text5";
+            this.text5.Size = new System.Drawing.Size(35, 13);
+            this.text5.TabIndex = 4;
+            this.text5.Text = "label3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 88);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(93, 71);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(186, 45);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Трудоемкость на 1 т железобетона, чел-дн.";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(93, 120);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(186, 45);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Трудоемкость на 1 т железобетона, чел-ч";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(93, 170);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(186, 44);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "Выработка на одного рабочего в смену, т";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(93, 218);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(186, 44);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Заработная плата на весь объем работа, руб";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox5.Location = new System.Drawing.Point(93, 266);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(186, 44);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.Text = "Заработная плата на 1 т железобетона, руб";
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 347);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text5);
+            this.Controls.Add(this.text4);
+            this.Controls.Add(this.text3);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form5";
+            this.Text = "Расчет показателей";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label text1;
+        private System.Windows.Forms.Label text2;
+        private System.Windows.Forms.Label text4;
+        private System.Windows.Forms.Label text3;
+        private System.Windows.Forms.Label text5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
